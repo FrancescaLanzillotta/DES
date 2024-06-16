78ffd870f12ac3bbaee46eb3b6af05a9ff46a97d
Francesca <francesca.lanzillotta@stud.unifi.it>
2024-06-16 17:59:04 +0200

    Added speedups

--- main.cu
+++ main.cu
@@ -31,8 +31,9 @@ int main() {
     bool saveResults = false;
     int N = 1000000;
     int length = 8;
-    int nCrack = 1000;
-    int nTests = 3;
+    vector<int> blockSizes = {32, 64, 128, 256};
+    int nCrack = 10;
+    int nTests = 10;
 
     uint64_t key = toUint64_T("aof3Ecp7");
 
@@ -50,10 +51,13 @@ int main() {
         }
         wordsFile.close();
 
+
         random_device rd;  // a seed source for the random number engine
         mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
         //gen.seed(42);
         uniform_int_distribution<> distrib(0, N);
+
+
 
         vector<uint64_t*> tests;
         for(int idTest = 0; idTest < nTests; idTest++){
@@ -70,57 +74,63 @@ int main() {
         cout << "\nSearch space: " << N;
         cout << "\nPasswords lengths: " << length;
         cout << "\nNumber of passwords to crack: " << nCrack;
+        cout << "\nBlock sizes to test: " << toString<int>(blockSizes);
         cout << "\nNumber of tests for each experiment: " << nTests;
         cout << "\n------------------ Sequential Experiment ------------------\n";
 
 
         vector<double> sTimes = {};
         for (auto &test: tests) {
-            cout << "Cracking password " << endl;
+            cout << "Test started " << endl;
             auto start = system_clock::now();
             for (int i = 0; i < nCrack; i++){
                 auto toCrack = desEncrypt(key, test[i]);
-//                for (int j = 0; j < N; j++){
-//                    if (toCrack == desEncrypt(key, pwdList[j]))
-//                        break;
-//                }
+                for (int j = 0; j < N; j++){
+                    if (toCrack == desEncrypt(key, pwdList[j]))
+                        break;
+                }
             }
             auto end = system_clock::now();
             auto seqElapsed = duration_cast<milliseconds>(end - start);
             sTimes.push_back((double)seqElapsed.count());
-            cout << "Password cracked (" << sTimes.back() << " ms)" <<  endl;
+            printf("Passwords cracked ( %f ms)\n", sTimes.back());
         }
         double sAvg = accumulate(sTimes.begin(), sTimes.end(), 0.0) / (double)sTimes.size();
-        cout << "Average time per experiment (ms): " << sAvg << endl;
+        printf("Average time per experiment (ms): %4.2f\n", sAvg);
 
 
         cout << "\n------------------ Parallel Experiment ------------------\n";
-        vector<int> blockSizes = {32, 64, 128, 256};
+
 
         vector<double> pAvg = {};
+        vector<double> speedUps = {};
         for (auto &blockSize: blockSizes) {
             printf("Block size: %d\n", blockSize);
             vector<double> pTimes = {};
             for (auto &test: tests) {
                 cout << "Test started" << endl;
-                int *found;
+                bool *found;
                 auto start = system_clock::now();
                 found = parallelCrack(pwdList, N, test, nCrack, key, blockSize);
                 auto end = system_clock::now();
-                auto seqElapsed = duration_cast<milliseconds>(end - start);
-                pTimes.push_back((double)seqElapsed.count());
-                cout << "Passwords cracked (" << pTimes.back() << " ms)" << endl;
+                auto parElapsed = duration_cast<milliseconds>(end - start);
+                pTimes.push_back((double)parElapsed.count());
+                printf("Passwords cracked ( %f ms)\n", pTimes.back());
                 for(int i = 0; i < nCrack; i++){
-                    if (found[i] == 0)
+                    if (!found[i])
                         printf("Error occurred");
                 }
                 free(found);
             }
             pAvg.push_back(accumulate(pTimes.begin(), pTimes.end(), 0.0) / (double)pTimes.size());
-            cout << "Average time per block size = " << blockSize << " (ms): " << pAvg.back() << endl;
+            speedUps.push_back(sAvg / pAvg.back());
+            printf("\nAverage time per block size = %d: %4.2f \n", blockSize, pAvg.back());
+            printf("\nSpeedup: %4.2fx\n", speedUps.back());
 
         }
 
+        cout << "\nAverage time per experiments (ms): " << toString<double>(pAvg);
+        cout << "\nSpeedups: " << toString<double>(speedUps);
 
 
         if (saveResults){
@@ -131,11 +141,13 @@ int main() {
             resultsFile << "\nNumber of passwords to crack: " << nCrack;
             resultsFile << "\nNumber of tests for each experiment: " << nTests;
             resultsFile << "\n------------------ Sequential Experiment ------------------";
-            //resultsFile << "\nAverage time per experiment (ms): " << sAvg;
+            resultsFile << "\nAverage time per experiment (ms): " << sAvg;
             resultsFile << "\n------------------ Parallel Experiment ------------------";
+            resultsFile << "\nBlock sizes tested: " << toString(blockSizes);
+            resultsFile << "\nAverage time per experiments (ms): " << toString(pAvg);
+            resultsFile << "\nSpeedups: " << toString(speedUps);
         }
         free(pwdList);
-        //free(pwdToCrack);
     } else {
         vector<string> words = wordsGeneration(N, length);
         ofstream wordsFile(wordsPath);
